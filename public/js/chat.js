6cd92ab101e434dcfb3cb289f9b940fbba7de992
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-05-22 23:58:35 -0400

    chat completo

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,31 @@
 (function () {
     const socket = io();
 
-    const usuarioActual = prompt('¿Cuál es tu nombre?').toUpperCase();
-
     const formulario = document.getElementById('formulario');
     const textarea = document.getElementById('textarea');
     const chat = document.getElementById('chat');
+    const modal = document.getElementById('modal');
+    const formularioLogin = document.getElementById('formulario-login');
+    const usuario = document.getElementById('usuario');
+    const color = document.getElementById('color');
+
+    let usuarioActual = {
+        nombre: '',
+        color: ''
+    }
+
+    formularioLogin.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        usuarioActual.nombre = usuario.value.trim();
+        usuarioActual.color = color.value.trim();
+
+        if (usuarioActual) {
+            modal.classList.add('modal--oculto');
+
+            textarea.focus();
+        }
+    });
 
     formulario.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -13,7 +33,7 @@
         const mensaje = textarea.value.trim()
 
         if (mensaje) {
-            socket.emit('chat', { usuario: usuarioActual, mensaje });
+            socket.emit('chat', { usuario: usuarioActual, mensaje: mensaje.replace(/\r?\n/g, '<br>') });
 
             textarea.value = '';
         }
@@ -28,10 +48,12 @@
     });
 
     socket.on('chat', ({ usuario, mensaje }) => {
+        const colorBorde = usuario.color ? `border-color: ${usuario.color}` : '';
+
         const burbuja = /*html*/ `
-            <div class="chat__fila ${usuario == usuarioActual ? 'chat__fila--izquierda' : 'chat__fila--derecha'}">
-                <div class="burbuja">
-                    <span class="burbuja__usuario">${usuario}</span>
+            <div class="chat__fila ${usuario.nombre == usuarioActual.nombre ? 'chat__fila--izquierda' : 'chat__fila--derecha'}">
+                <div class="burbuja" style="${colorBorde}">
+                    <span class="burbuja__usuario">${usuario.nombre}</span>
                     <p class="burbuja__texto">
                         ${mensaje}
                     </p>
